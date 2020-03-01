b24e2fc768ac53a1524192a28f3c56a0a9253338
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 13:46:24 -0500

    add: some styling and other bullshit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <header class="bg-blue-400 shadow-lg">
-      <h3 class="text-xl">Who Lives In A Pineapple Under the Sea?</h3>
+  <div
+    class="bg-scroll bg-center bg-cover h-screen"
+    style="background-image: url('underwater.jpg')"
+  >
+    <header class="bg-blue-700 shadow text-white font-bold">
+      <h3 class="text-xl text-center pt-4">
+        SpongeBob SquarePants Trivia
+      </h3>
+      <h6 class="text-center text-sm font-normal pb-4">
+        | Who lives in a pineapple under the sea? |
+      </h6>
     </header>
     <nuxt />
   </div>
