217f0e209810a0b11f483969efacb96540f1cbd8
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 13:50:49 -0500

    add: color change on button

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex flex-col spacy-y-4 space-x-4">
     <nuxt-link to="/quiz">
-      <button class="bg-gray-400 text-white rounded m-4 py-1 px-2 shadow">Start</button>
+      <button class="bg-gray-600 text-white rounded m-4 py-1 px-2 shadow">Start</button>
     </nuxt-link>
   </section>
 </template>
