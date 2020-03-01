7646afdbf42101da5b718a0d80a972117fa0609a
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 13:45:40 -0500

    add: key to question options in `<questions.js>`

--- util/questions.js
+++ util/questions.js
@@ -6,41 +6,50 @@ export default {
       options: [
         {
           optionValue: "SpongeBob SquarePants",
-          truthValue: "1"
+          truthValue: "1",
+          key: 11
         },
         {
           optionValue: "Mr. Krabs",
-          truthValue: "0"
+          truthValue: "0",
+          key: 12
         },
         {
           optionValue: "Patrick Star",
-          truthValue: "0"
+          truthValue: "0",
+          key: 13
         },
         {
           optionValue: "Squidward Tentacles",
-          truthValue: "0"
+          truthValue: "0",
+          key: 14
         }
       ]
     },
     {
       id: 2,
-      questionValue: "How many fingers and thumbs does SpongeBob Squarepants have?",
+      questionValue:
+        "How many fingers and thumbs does SpongeBob Squarepants have?",
       options: [
         {
           optionValue: "6",
-          truthValue: "0"
+          truthValue: "0",
+          key: 21
         },
         {
           optionValue: "8",
-          truthValue: "1"
+          truthValue: "1",
+          key: 22
         },
         {
           optionValue: "10",
-          truthValue: "0"
+          truthValue: "0",
+          key: 23
         },
         {
           optionValue: "12",
-          truthValue: "0"
+          truthValue: "0",
+          key: 24
         }
       ]
     },
@@ -50,41 +59,50 @@ export default {
       options: [
         {
           optionValue: "Hector",
-          truthValue: "0"
+          truthValue: "0",
+          key: 31
         },
         {
           optionValue: "Hart",
-          truthValue: "0"
+          truthValue: "0",
+          key: 32
         },
         {
           optionValue: "Herb",
-          truthValue: "1"
+          truthValue: "1",
+          key: 33
         },
         {
           optionValue: "Harry",
-          truthValue: "0"
+          truthValue: "0",
+          key: 34
         }
       ]
     },
     {
       id: 4,
-      questionValue: "Before Mr. Krabs bought the Krusty Krab it was called what ? ",
+      questionValue:
+        "Before Mr. Krabs bought the Krusty Krab it was called what ? ",
       options: [
         {
           optionValue: "The Krooked Krab",
-          truthValue: "0"
+          truthValue: "0",
+          key: 41
         },
         {
           optionValue: "The Busty Krab",
-          truthValue: "0"
+          truthValue: "0",
+          key: 42
         },
         {
           optionValue: "The Musty Krab",
-          truthValue: "0"
+          truthValue: "0",
+          key: 43
         },
         {
           optionValue: "The Rusty Krab",
-          truthValue: "1"
+          truthValue: "1",
+          key: 44
         }
       ]
     },
@@ -94,19 +112,23 @@ export default {
       options: [
         {
           optionValue: "Coral Kid",
-          truthValue: "0"
+          truthValue: "0",
+          key: 51
         },
         {
           optionValue: "Sir Sand",
-          truthValue: "0"
+          truthValue: "0",
+          key: 52
         },
         {
           optionValue: "Patrick Star",
-          truthValue: "0"
+          truthValue: "0",
+          key: 53
         },
         {
           optionValue: "Barnacle Boy",
-          truthValue: "1"
+          truthValue: "1",
+          key: 54
         }
       ]
     }
