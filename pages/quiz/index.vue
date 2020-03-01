a6959ca1f7b6d7a37fe35c75a657141edecbc059
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 13:51:43 -0500

    add: <QuestionTimer> logic and import to quiz page

---
+++ pages/quiz/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="p-8 flex flex-col w-full antialiased text-center items-center justify-center"
+  >
+    <QuestionTimer :time-left="timeLeft" :time-limit="timeLimit" />
+    <QuizQuestion :questions="questions" />
+  </div>
+</template>
+
+<script>
+import QuestionTimer from "@/components/QuestionTimer";
+import QuizQuestion from "@/components/QuizQuestion";
+import questions from "@/util/questions.js";
+
+const TIME_LIMIT = 20;
+
+export default {
+  components: {
+    QuestionTimer,
+    QuizQuestion
+  },
+
+  data() {
+    return {
+      timePassed: 0,
+      timerInterval: null,
+      questions: []
+    };
+  },
+
+  computed: {
+    timeLimit() {
+      return TIME_LIMIT;
+    },
+
+    timeLeft() {
+      return this.timeLimit - this.timePassed;
+    }
+  },
+
+  watch: {
+    timeLeft(newValue) {
+      if (newValue === 0) {
+        this.onTimesUp();
+      }
+    }
+  },
+
+  mounted() {
+    this.startTimer();
+    this.questions = questions.questions;
+  },
+
+  methods: {
+    onTimesUp() {
+      clearInterval(this.timerInterval);
+    },
+
+    startTimer() {
+      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
+    }
+  }
+};
+</script>
