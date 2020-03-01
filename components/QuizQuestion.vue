011510fc37d2769d126dde5d4b53b9417861db29
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 13:50:23 -0500

    add: display questions with v-for loop

--- components/QuizQuestion.vue
+++ components/QuizQuestion.vue
@@ -1,8 +1,24 @@
-<template></template>
+<template>
+  <section class="text-white m-8">
+    <div v-for="question in questions" :key="question.id">
+      {{ question.id }}.
+      {{ question.questionValue }}
+      <div v-for="option in question.options" :key="option.key">
+        {{ option.optionValue }}
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
-import questions from "@/util/questions";
-import QuestionTimer from "@/components/QuestionTimer";
+import questions from "@/util/questions.js";
 
-export default {};
+export default {
+  props: {
+    questions: {
+      type: Array,
+      required: true
+    }
+  }
+};
 </script>
