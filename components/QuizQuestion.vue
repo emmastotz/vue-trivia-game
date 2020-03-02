49d6c5766c1b68ce697b18d8c7988243f84b4413
Beaverhausen <stotz.emma@gmail.com>
2020-03-01 21:10:23 -0500

    edit: semantically correct html tags

--- components/QuizQuestion.vue
+++ components/QuizQuestion.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
-    <button class="bg-gray-600 text-white rounded m-4 py-1 px-2 shadow" @click= "next" > Submit Answer </button>
+  <section>
     <form id="q-form">
-    <section class="text-white m-8">
-      <div v-if="questions[index] != undefined">
-        <p> {{ questions[index].id }}.{{ questions[index].questionValue }}</p>
-        <div v-for="option in questions[index].options" :key="option.key" class="custom-control custom-radio custom-control-inline">
-          <input type="radio" :id= "option.key" :value= "option.truthValue" class="custom-control-input" :for="questions[index].id" :name="questions[index].id">
-          <label :for= "option.key" class="custom-control-label"> {{option.optionValue}}  </label>
+      <div class="text-white m-8 blue rounded-lg shadow px-6 pt-4">
+        <div v-if="questions[index] != undefined">
+          <p>{{ questions[index].id }}. {{ questions[index].questionValue }}</p>
+          <div v-for="option in questions[index].options" :key="option.key" class>
+            <input
+              type="radio"
+              :id="option.key"
+              :value="option.truthValue"
+              :for="questions[index].id"
+              :name="questions[index].id"
+            />
+            <label :for="option.key">{{option.optionValue}}</label>
+          </div>
         </div>
+        <button
+          class="bg-gray-600 text-white rounded m-4 py-1 px-2 shadow focus:outline-none hover:bg-gray-700"
+          @click.prevent="next"
+        >Submit</button>
       </div>
-    </section>
     </form>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -25,7 +34,7 @@ export default {
     };
   },
   mounted() {
-    this.index = 0
+    this.index = 0;
   },
   props: {
     questions: {
@@ -34,7 +43,7 @@ export default {
     }
   },
   methods: {
-    next(){
+    next() {
       this.index++;
     }
   }
